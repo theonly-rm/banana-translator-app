680229eb1765bf9ea1d3b3e280e82020095db814
Rachit Mahajan <141140128+theonly-rm@users.noreply.github.com>
2023-12-21 19:43:31 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 let btnTranslate = document.querySelector("#btn-translate");
 let txtInput = document.querySelector("#txt-input");
+let outputDiv = document.querySelector("#output");
+
+// outputDiv.innerText="Rachit Mahajan"
+// function clickHandler() {
+//     console.log("clicked");
+//     console.log("input:", txtInput.value);
+//     console.log("")
+// }
 
 function clickHandler() {
-    console.log("clicked");
-    console.log("input:", txtInput.value);
+    outputDiv.innerText="abchdkritejwmpxoanwgfure " + txtInput.value;
 }
 
 btnTranslate.addEventListener("click", clickHandler);

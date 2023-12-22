44325abd18c10a160905d77b444c1abd10927abc
Rachit Mahajan <141140128+theonly-rm@users.noreply.github.com>
2023-12-22 23:47:49 +0530

    made the complete js logic

--- app.js
+++ app.js
@@ -9,8 +9,33 @@ let outputDiv = document.querySelector("#output");
 //     console.log("")
 // }
 
-function clickHandler() {
-    outputDiv.innerText="abchdkritejwmpxoanwgfure " + txtInput.value;
-}
+
+
 
 btnTranslate.addEventListener("click", clickHandler);
+
+//fetch
+
+
+let serverURL = "https://api.funtranslations.com/translate/navi.json";
+
+function getTranslationURL (text) {
+    return serverURL + "?text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("error occured" , error)
+    alert("something is wrong in the server, try again after sometime")
+}
+
+function clickHandler() {
+    let inputText = txtInput.value;                    //input
+
+fetch(getTranslationURL(inputText))        //calling server for processing      
+    .then(response => response.json())
+    .then(json => {
+        let translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;             //output
+    })
+    .catch(errorHandler)
+};    
